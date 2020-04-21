ee47269258660f4f3ab1f3d53db979c810fe7549
dp4wn <63305212+dp4wn@users.noreply.github.com>
2020-04-20 22:41:12 -0400

    Add files via upload
    updated

--- final project.R
+++ final project.R
@@ -2,31 +2,30 @@ library(ggplot2)
 library(usmap)
 
 #plotting and finding correlation between severe housing issues and SOL pass rate
-ggplot(data=binned_data, aes(x=binned_data$Severe_Housing_Issues, y=binned_data$`SOL PassRate`))+ geom_point()
-cor(binned_data$Severe_Housing_Issues,binned_data$`SOL PassRate`)
+ggplot(data=binned_data_final, aes(x=binned_data_final$Severe_Housing_Issues, y=binned_data_final$`SOL PassRate`))+ geom_point()
+cor(binned_data_final$Severe_Housing_Issues,binned_data_final$`SOL PassRate`)
 
 #plotting and finding correlation between severe housing issues and SOL advanced pass rate
-ggplot(data=binned_data, aes(x=binned_data$Severe_Housing_Issues, y=binned_data$`SOL AdvPassRate`))+ geom_point()
-cor(binned_data$Severe_Housing_Issues,binned_data$`SOL PassRate`)
+ggplot(data=binned_data_final, aes(x=binned_data_final$Severe_Housing_Issues, y=binned_data_final$`SOL AdvPassRate`))+ geom_point()
+cor(binned_data_final$Severe_Housing_Issues,binned_data_final$`SOL PassRate`)
 
 #plotting and finding correlation between child mortality and SOL pass rate
-ggplot(data=binned_data, aes(x=binned_data$Child_Mortality, y=binned_data$`SOL PassRate`))+ geom_point()
-cor(binned_data$Child_Mortality,binned_data$`SOL AdvPassRate`)
+ggplot(data=binned_data_final, aes(x=binned_data_final$Child_Mortality, y=binned_data_final$`SOL PassRate`))+ geom_point()
+cor(binned_data_final$Child_Mortality,binned_data_final$`SOL AdvPassRate`)
 
 #plotting and finding correlation between child mortality and SOL advanced pass rate
-ggplot(data=binned_data, aes(x=binned_data$Child_Mortality, y=binned_data$`SOL AdvPassRate`))+ geom_point()
-cor(binned_data$Child_Mortality,binned_data$`SOL AdvPassRate`)
+ggplot(data=binned_data_final, aes(x=binned_data_final$Child_Mortality, y=binned_data_final$`SOL AdvPassRate`))+ geom_point()
+cor(binned_data_final$Child_Mortality,binned_data_final$`SOL AdvPassRate`)
 
 ##mapping
-data <- data.frame(county = binned_data$County, values = binned_data$Severe_Housing_Issues)
+data <- data.frame(county = binned_data_final$County, values = binned_data_final$Severe_Housing_Issues)
 
 va <- us_map(include = "VA",regions = "counties")
-loco <- unique(va$county)
-dataset <- cbind(loco,binned_data$Severe_Housing_Issues)
+dataset <- cbind(binned_data_final$County,binned_data_final$Severe_Housing_Issues)
 x <- data.frame(dataset)
 
 
-fippy <- fips("VA", county = x$loco)
+fippy <- fips("VA", county = x$X1)
 new_x<- data.frame(x,fippy)
 newnew_x <- data.frame(new_x[,c(2,3)])
 names(newnew_x) <- c("vals","fips")
